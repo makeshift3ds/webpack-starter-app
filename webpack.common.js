c0a9187c53500e1e62eb0f860cab10322799f5f5
makeshift3ds <makeshift3ds@gmail.com>
2017-10-16 15:16:11 -0400

    add clean-webpack-plugin to annhilate the dist folder

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
@@ -27,6 +28,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin("dist"),
     new HtmlWebpackPlugin({
       template: "src/index.html"
     })
