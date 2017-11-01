0816cb9dd750b59860ef9120163ca61da73eaf15
Ken Elliott <makeshift3ds@gmail.com>
2017-11-01 11:29:53 -0400

    add webpack-dashboard

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,8 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common");
+const DashboardPlugin = require("webpack-dashboard/plugin");
+
+common.plugins.push(new DashboardPlugin());
 
 module.exports = merge(common, {
   devServer: {
